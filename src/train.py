2ccaddcd1393b5a916dfc8923e9071f037d5c4f0
Samsella <untergasser@uni-potsdam.de>
2019-02-05 11:22:27 +0100

    bug fixes5

--- src/train.py
+++ src/train.py
@@ -9,6 +9,7 @@ from keras.models import load_model
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 from keras.losses import sparse_categorical_crossentropy as scc
+#from tensorflow.nn import sparse_softmax_cross_entropy_with_logits as tfscc
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -20,11 +21,14 @@ def train():
              path2data+'europarl-v7.de-en.en']
     d = data(files, size=4000)
 
-    if not os.path.exists('data.npz'):
-        data_X, data_y, labels = d.get_data(test=1)
+    test = 1
+
+    if not os.path.exists('data.npz') or test:
+        data_X, data_y, labels = d.get_data(test=test)
         np.savez_compressed('data', a=data_X, b=data_y, c=labels)
     else:
-        data_X, data_y, labels = [arr for key, arr in np.load('data.npz').items()]
+        d = np.load('data.npz')
+        data_X, data_y, labels = d['data_X'], d['data_y'], d['labels']
 
     train_X, test_X, train_y, test_y, train_labels, test_labels= train_test_split(data_X, data_y, labels, test_size=0.002, random_state=13)
     print(train_X[150:151])
@@ -41,11 +45,10 @@ def train():
         '''
         return scc(y_true, y_pred)
 
-    callback_checkpoint = ModelCheckpoint(filepath='chpt.keras',
+    callback_checkpoint = ModelCheckpoint(filepath='chpt{epoch:02d}.keras',
                                       monitor='val_loss',
                                       verbose=0,
-                                      save_weights_only=True,
-                                      save_best_only=True)
+                                      save_weights_only=True)
 
 
     callback_early_stopping = EarlyStopping(monitor='val_loss',
@@ -64,16 +67,16 @@ def train():
     sn.compile('Adam', sl)
     model = sn.model
 
-    if os.path.exists('chpt.keras'):
-        model.load_weights('chpt.keras')
+    if os.path.exists('chpt20.keras'):
+        model.load_weights('chpt20.keras')
         print('Weights loaded')
 
-    model.fit(x=[train_X, train_y], y=train_labels, batch_size=64, epochs=10,
+    model.fit(x=[train_X, train_y], y=train_labels, batch_size=16, epochs=10,
               verbose=2, validation_split=0.002, callbacks=callbacks)
 
-    print(test_y[:1])
-    print(d.decode(np.array(test_y[:1]), 'de'))
-    p = sn.predict(test_X[:1])
+    print(test_y[1,:])
+    print(d.decode(np.array(test_y[1,:]), 'de'))
+    p = sn.predict(test_X[1,:])
     print(p)
     print(d.decode(np.array(p), 'de'))
 
